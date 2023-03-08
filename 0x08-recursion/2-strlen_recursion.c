5d8aa5ee2117ded42877435aab0f4b16d58dae6c
TimmyMuema <muematimmy@gmail.com>
2023-03-08 04:19:58 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,18 +2,15 @@
 
 /**
  * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- * Return: length of string
+ * @s: the string to calculate the length of
+ *
+ * Return: the length of the string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		return 0;
-	}
-	else
-	{
-		return (_strlen_recursion(s + 1) + 1);
-	}
+		return (0);
+
+	return (_strlen_recursion(s + 1) + 1);
 }
 
