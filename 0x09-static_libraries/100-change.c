f8628e28e39c436610a782e4327786e38dda9224
TimmyMuema <muematimmy@gmail.com>
2023-03-10 14:58:05 -0800

    100-change.c

--- 0x09-static_libraries/100-change.c
+++ 0x09-static_libraries/100-change.c
@@ -2,34 +2,48 @@
 #include <stdlib.h>
 
 /**
- * main - print the minimum number of coins to make change
- * @argc: int
- * @argv: string
- * Return: int
+ * main - prints the minimum number of coins to make change for an amount of money
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments passed to the program
+ *
+ * Return: 0 on success, 1 on error
  */
-
 int main(int argc, char *argv[])
 {
-	int i, n, s, ch = 0;
-	int c[] = {25, 10, 5, 2, 1};
+    int coins = 0, cents;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	s = atoi(argv[1]);
-	if (s < 1)
-		printf("0\n");
-	else
-	{
-		for (i = 0; i < 5 && s; i++)
-		{
-			n = s / c[i];
-			ch += n;
-			s -= n * c[i];
-		}
-		printf("%d\n", ch);
-	}
-	return (0);
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    cents = atoi(argv[1]);
+
+    if (cents < 0)
+    {
+        printf("0\n");
+        return (0);
+    }
+
+    while (cents > 0)
+    {
+        if (cents >= 25)
+            cents -= 25;
+        else if (cents >= 10)
+            cents -= 10;
+        else if (cents >= 5)
+            cents -= 5;
+        else if (cents >= 2)
+            cents -= 2;
+        else
+            cents -= 1;
+
+        coins++;
+    }
+
+    printf("%d\n", coins);
+
+    return (0);
 }
+
